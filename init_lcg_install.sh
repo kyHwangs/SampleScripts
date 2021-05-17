c632be9121ccdb866a2c0da3044621a77347437d
kyHwangs <kyuyeong.hwang@gmail.com>
2021-05-17 09:16:52 +0900

    initial commit

---
+++ init_lcg_install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+source /cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_1/x86_64-centos7-gcc8-opt/setup.sh
+export PYTHIA8_ROOT_DIR=/cvmfs/sft.cern.ch/lcg/views/LCG_97a_FCC_1/x86_64-centos7-gcc8-opt/
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_97a_FCC_1/hepmc3/3.2.2/x86_64-centos7-gcc8-opt/hepmc3-env.sh
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_97a_FCC_1/DD4hep/01-12-01/x86_64-centos7-gcc8-opt/bin/thisdd4hep.sh
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/lib
